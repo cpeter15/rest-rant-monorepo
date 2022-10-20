530243dfa6c880f1b2c501e27207228539626c71
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-10-20 19:53:04 -0400

    added session cookies

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt')
 const { User } = db
 
 
+
 router.post('/', async (req, res) => {
 
     let user = await User.findOne({
@@ -16,9 +17,26 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password`
         })
     } else {
+        req.session.userId = user.userId
         res.json({ user })
     }
 })
+
+
+router.get('/profile', async (req, res) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                userId: req.session.userId
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
+
+
 
 
 
