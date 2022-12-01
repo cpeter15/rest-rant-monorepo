0b7550447eac2e8f7b29938bc2936c8b355aff49
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-12-01 18:32:06 -0500

    built frontend into backend

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
+const path = require('path')
 
 // Express Settings
 app.use(cors())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+// serve static front end in production mode
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+}
 
 // Controllers & Routes
 
@@ -18,6 +24,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/places', require('./controllers/places'))
 app.use('/users', require('./controllers/users'))
 app.use('/authentication', require('./controllers/authentication'))
+
+
 
 // Listen for Connections
 app.listen(process.env.PORT, () => {
